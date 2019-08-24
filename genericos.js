b31c5fc443312625910c751c97d2d4a94ec76c94
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 23:31:36 -0600

    parametro a funciones

--- genericos.js
+++ genericos.js
@@ -6,3 +6,10 @@ var Generic = /** @class */ (function () {
 var myGeneric = new Generic();
 console.log(myGeneric.add = function (x, y) { return x + y; });
 console.log(myGeneric.add(3, 4));
+function echo(arg) {
+    console.log(arg.length);
+    return arg;
+}
+var a = echo("aaa");
+var t = echo({ length: 2, name: "aa" });
+//let b = echo(1);
--- genericos.ts
+++ genericos.ts
@@ -6,3 +6,17 @@ let myGeneric = new Generic<number>();
 
 console.log(myGeneric.add = function (x,y) {return x + y});
 console.log(myGeneric.add(3,4));
+
+interface withLength{
+    length: number;
+}
+
+function echo<T extends withLength>(arg: T): T{
+    console.log(arg.length);
+    return arg;
+}
+
+let a = echo("aaa");
+let t = echo({length: 2, name: "aa"});
+
+//let b = echo(1);
