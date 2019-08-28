018b2ebbd4d79a2093bef0b21949873b16871cf7
fherneftali <fhernftali.fj@gmail.com>
2019-08-27 23:54:37 -0600

    promise catch

--- promesas.js
+++ promesas.js
@@ -1,15 +1,11 @@
-Promise.resolve(123)
+Promise.reject(new Error('something bad happened'))
     .then(function (res) {
     console.log(res);
     return 456;
-})
-    .then(function (res) {
-    console.log(res);
-    return Promise.resolve(123);
-})
-    .then(function (res) {
-    console.log(res);
-    return Promise.resolve(123);
 })["catch"](function (err) {
     console.log(err.message);
+    return Promise.resolve(123);
+})
+    .then(function (res) {
+    console.log(res);
 });
--- promesas.ts
+++ promesas.ts
@@ -1,16 +1,12 @@
- Promise.resolve(123)
+ Promise.reject(new Error('something bad happened'))
     .then((res) => {
         console.log(res);
         return 456;
     })
-    .then((res) => {
-        console.log(res);
-        return Promise.resolve(123);
-    })
-    .then((res) => {
-        console.log(res);
-        return Promise.resolve(123);
-    })
     .catch((err) => {
         console.log(err.message);
+        return Promise.resolve(123);
+    })
+    .then((res) => {
+        console.log(res);      
     });
