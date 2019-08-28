f06e9ff54474d7c22dbddf5d0c83b9bc01d4ea03
fherneftali <fhernftali.fj@gmail.com>
2019-08-28 00:09:07 -0600

    carga de JSON

---
+++ cargaJson.ts
@@ -0,0 +1,22 @@
+//good json file
+loadJSONAsync('good.json')
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('good.json error', err.message);
+    })
+//non-existent json file
+    .then(function() {
+        return loadJSONAsync('absent.json');
+    })
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('absent.json error', err.message);
+    })
+//invalid json file
+    .then(function() {
+        return loadJSONAsync('bad.json');
+    })
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('bad.json error', err.message);
+    })
