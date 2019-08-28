859b9d7cf15a370d9219168f7df24e99e3009f80
fherneftali <fhernftali.fj@gmail.com>
2019-08-27 23:29:51 -0600

    Desestructuracion de objetos y arrays

---
+++ desestructuracion.ts
@@ -0,0 +1,11 @@
+var obj = {x :1, y: 2, z: 3};
+console.log(obj.x);
+
+var {x, y, z} = obj;
+console.log(x);
+
+var array = [1, 2, 3];
+console.log(array[0]);
+
+var [x, y, z] = array;
+console.log(x);
