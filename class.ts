230c7ba86a884eab855b9079a797f598dc0f6109
fherneftali <fhernftali.fj@gmail.com>
2019-09-01 15:44:17 -0600

    IIFE

--- class.ts
+++ class.ts
@@ -1,19 +1,23 @@
-class FooBase {
-    public x: number;
-    private y: number;
-    protected z: number;
+class Point {
+    x: number;
+    y: number;
+    constructor (x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    add(point: Point) {
+        return new Point(this.x + point.x, this.y + point.y);
+    }
 }
 
-var foo = new FooBase();
-foo.x;
-foo.y;
-foo.z;
-
-class FooChild extends FooBase {
-    constructor() {
-        super();
-        this.x;
-        this.y;
-        this.z;
+class Point3D extends Point {
+    z: number;
+    constructor(x: number, y: number, z: number) {
+        super(x, y);
+        this.z = z;
+    }
+    add(point: Point3D) {
+        var point2D = super.add(point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
     }
 }
